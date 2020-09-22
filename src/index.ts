77b876d90909ee04e9d4413382ba767bafd2ef4d
Dominik Szamburski <dominikszamburski99@gmail.com>
2020-09-22 08:13:17 +0200

    add code linter && remove IResponse interface

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,30 @@
-import * as amqp from 'amqplib';
-import Request from './request';
-import Response from './response';
+import * as amqp from 'amqplib'
+import Request from './request'
+import Response from './response'
 
-export class EventDispatcher
-{
+export class EventDispatcher {
     private req?: Request
     private res?: Response
 
-    constructor () {}
+    public static create = async (url: string): Promise<EventDispatcher> => {
+      const self = new EventDispatcher()
+      const conn = await amqp.connect(url)
 
-    public static create = async (url: string) => {
-        const self = new EventDispatcher()
-        const conn = await amqp.connect(url)
+      self.req = new Request(conn)
+      self.res = new Response(conn)
 
-        self.req = new Request(conn)
-        self.res = new Response(conn)
-
-        return self
+      return self
     }
 
-    public async emit(event: string, ...args: any[]): Promise<any>
-    {
-        return this.req?.emit(event, args)
+    public async emit (event: string, ...args: any[]): Promise<any> {
+      return this.req?.emit(event, args)
     }
 
-    public register (event: string, callback: (...args: any[]) => any)
-    {
+    public register (event: string, callback: (...args: any[]) => any): void {
         this.res?.register(event, callback)
     }
 
-    public async listen ()
-    {
+    public async listen (): Promise<void> {
         this.res?.listen()
     }
 }
--- src/request.ts
+++ src/request.ts
@@ -1,71 +1,71 @@
-import {ConsumeMessage, Connection, Channel} from 'amqplib';
+import { ConsumeMessage, Connection, Channel } from 'amqplib'
 
+export interface IRequest {
+    event: string,
+    args?: any[]
+}
 
-export default class Request
-{
+export default class Request {
     private readonly connection: Connection
     private readonly queue: string = 'request'
-    private requests: Map<string, object> = new Map();
+    private requests: Map<string, unknown> = new Map();
 
-    constructor(connection: Connection)
-    {
-        this.connection = connection
-        this.requests = new Map()
+    constructor (connection: Connection) {
+      this.connection = connection
+      this.requests = new Map()
     }
 
     // Public API
-    public async emit(event: string, ...args: any[]): Promise<any>
-    {
-        const channel = await this.connection.createChannel();
-        const { queue } = await channel.assertQueue('', { exclusive: true });
+    public async emit (event: string, ...args: any[]): Promise<any> {
+      const channel = await this.connection.createChannel()
+      const { queue } = await channel.assertQueue('', { exclusive: true })
 
-        const correlationId = this.generateUUID();
-        const payload = Buffer.from(JSON.stringify({ event, args}));
-        const options = { correlationId,  replyTo: queue };
+      const correlationId = this.generateUUID()
+      const payload = Buffer.from(JSON.stringify({ event, args }))
+      const options = { correlationId, replyTo: queue }
 
-        this.requests.set(correlationId, { payload, options });
-        channel.sendToQueue(this.queue, payload, options);
+      this.requests.set(correlationId, { payload, options })
+      channel.sendToQueue(this.queue, payload, options)
 
-        return Promise.race([
-            this.getResponse(queue, channel),
-            this.callTimeout(correlationId)
-        ]).catch(e => {
-            channel.close();
-            throw e;
-        }); 
+      return Promise.race([
+        this.getResponse(queue, channel),
+        this.callTimeout(correlationId)
+      ]).catch(e => {
+        channel.close()
+        throw e
+      })
     }
 
     // Private API
-    private callTimeout (correlationId: string): Promise<void> 
-    {
-        return new Promise((_, reject) => {
-            setTimeout(() => {
-                this.requests.delete(correlationId);
-                reject('[Dispatcher] Timeout');
-            }, 25000);
-        });
+    private callTimeout (correlationId: string): Promise<void> {
+      return new Promise((_, reject) => {
+        setTimeout(() => {
+          this.requests.delete(correlationId)
+          reject('[Dispatcher] Timeout')
+        }, 25000)
+      })
     }
-    private getResponse (queue: string, channel: Channel): Promise<any> {
-        return new Promise (async (resolve, _) => {
-            await channel.consume(queue, (message: ConsumeMessage | null) => {
-                if (!message) return;
 
-                const { correlationId } = message.properties;
-                const callback = this.requests.get(correlationId);
-    
-                if (!callback) {
-                    console.log('[Dispatcher] Unknown event.');
-                    return;
-                }
-    
-                const response = JSON.parse(message.content.toString());
-                resolve(response);
-            });
+    private getResponse (queue: string, channel: Channel): Promise<any> {
+      return new Promise((resolve, _) => {
+        channel.consume(queue, (message: ConsumeMessage | null) => {
+          if (!message) return
+
+          const { correlationId } = message.properties
+          const callback = this.requests.get(correlationId)
+
+          if (!callback) {
+            console.log('[Dispatcher] Unknown event.')
+            return
+          }
+
+          const response = JSON.parse(message.content.toString())
+          resolve(response)
         })
+      })
     }
-    
-    private generateUUID () : string 
-    {
-        return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+
+    private generateUUID () : string {
+      return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
     }
 }
