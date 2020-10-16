aa8327432d67a5104f06d408a63eb4bef6db5b3c
Dominik Szamburski <dominikszamburski99@gmail.com>
2020-10-16 12:54:44 +0200

    0.3.0

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,10 @@
-const { time } = require('console')
 const crypto = require('crypto')
 
 exports.generateUUID = () => {
-    const hash = crypto.createHash('sha256')
+  const hash = crypto.createHash('sha256')
 
-    const timestamp = Date.now()
-    const random = Math.random().toString(2, 15)
+  const timestamp = Date.now()
+  const random = Math.random().toString(2, 15)
 
-    return hash.update(`${timestamp}-${random}`).digest('hex')
+  return hash.update(`${timestamp}-${random}`).digest('hex')
 }
